5fdfe87c84c40e9de52df9577cf99574addce3de
john <wildfireone@gmail.com>
2018-02-15 13:49:42 +0000

    fixed npm

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const http = require('http'),
   fs = require('fs');
   var querystring = require('querystring');
   var url = require('url');
-  const port =80
+  const port =8080
 
 var products =["Pear myPhone", "bixel2","2+3 haxxor phone"];
 
